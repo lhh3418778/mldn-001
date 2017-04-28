149d7b53dfa75e1292da22774e469636a823c6fe
mldn <1974605717@qq.com>
2017-04-29 00:09:45 +0800

    reset Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
 	public static void main(String [] args) {
 		System.out.print("hello world!!!! ") ;
+		System.out.print("hello world!!!! ") ;
+		System.out.print("hello world!!!! ") ;
+
 	}
 }
